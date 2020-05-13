43d8a2d184e77a787946e7e3d3289b47522980af
Rafal-Gryczka <63900821+Rafal-Gryczka@users.noreply.github.com>
2020-05-13 22:15:31 +0200

    add search function

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-const toDoList = [];
+let toDoList = [];
 
 const input = document.body.querySelector("input");
+const inputSearch = document.body.querySelector('input.search');
 const form = document.body.querySelector('form');
 const taskNumber = document.body.querySelector('h1 span');
 const btnDelete = document.body.querySelector('button.delete');
 const ul = document.body.querySelector('ul');
+
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let cloneToDoList = [...toDoList];
+    cloneToDoList = cloneToDoList.filter(li => li.textContent.toLowerCase().includes(searchText));
+    taskNumber.textContent = cloneToDoList.length;
+
+    if (inputSearch.value === "") {
+        renderList();
+    } else {
+        ul.textContent = "";
+        cloneToDoList.forEach(li => ul.appendChild(li));
+    }
+
+}
 
 const deleteAll = () => {
     toDoList.splice(0, toDoList.length);
@@ -53,3 +69,4 @@ const renderList = () => {
 
 form.addEventListener('submit', addTask);
 btnDelete.addEventListener('click', deleteAll);
+inputSearch.addEventListener('input', searchTask);
