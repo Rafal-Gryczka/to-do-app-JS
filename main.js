70c62975267f4528b3f6e0bc2658f1c767b25fc3
Rafal-Gryczka <63900821+Rafal-Gryczka@users.noreply.github.com>
2020-05-14 21:25:09 +0200

    add cleaning search input after task delete

--- main.js
+++ main.js
@@ -1,72 +1,67 @@
 let toDoList = [];
 
 const input = document.body.querySelector("input");
-const inputSearch = document.body.querySelector('input.search');
-const form = document.body.querySelector('form');
-const taskNumber = document.body.querySelector('h1 span');
-const btnDelete = document.body.querySelector('button.delete');
-const ul = document.body.querySelector('ul');
+const inputSearch = document.body.querySelector("input.search");
+const form = document.body.querySelector("form");
+const taskNumber = document.body.querySelector("h1 span");
+const btnDelete = document.body.querySelector("button.delete");
+const ul = document.body.querySelector("ul");
 
 const searchTask = (e) => {
-    const searchText = e.target.value.toLowerCase();
-    let cloneToDoList = [...toDoList];
-    cloneToDoList = cloneToDoList.filter(li => li.textContent.toLowerCase().includes(searchText));
-    taskNumber.textContent = cloneToDoList.length;
+  const searchText = e.target.value.toLowerCase();
+  let cloneToDoList = [...toDoList];
+  cloneToDoList = cloneToDoList.filter((li) =>
+    li.textContent.toLowerCase().includes(searchText)
+  );
+  taskNumber.textContent = cloneToDoList.length;
 
-    if (inputSearch.value === "") {
-        renderList();
-    } else {
-        ul.textContent = "";
-        cloneToDoList.forEach(li => ul.appendChild(li));
-    }
-
-}
+  if (inputSearch.value === "") {
+    renderList();
+  } else {
+    ul.textContent = "";
+    cloneToDoList.forEach((li) => ul.appendChild(li));
+  }
+};
 
 const deleteAll = () => {
-    toDoList.splice(0, toDoList.length);
-    taskNumber.textContent = toDoList.length;
-    input.value = "";
-    renderList();
-
-
-}
+  toDoList.splice(0, toDoList.length);
+  taskNumber.textContent = toDoList.length;
+  input.value = "";
+  renderList();
+};
 
 const removeTask = (e) => {
-    e.target.parentNode.remove();
-    const index = e.target.parentNode.dataset.key;
-    toDoList.splice(index, 1);
-    taskNumber.textContent = toDoList.length;
-    renderList();
-
-}
+  e.target.parentNode.remove();
+  const index = e.target.parentNode.dataset.key;
+  toDoList.splice(index, 1);
+  taskNumber.textContent = toDoList.length;
+  renderList();
+  inputSearch.value = "";
+};
 
 const addTask = (e) => {
+  e.preventDefault();
+  const taskTitle = input.value;
+  if (taskTitle === "") return;
+  const task = document.createElement("li");
+  task.className = "task";
+  task.innerHTML = `${taskTitle} <button>X</button>`;
+  toDoList.push(task);
+  renderList();
 
-    e.preventDefault();
-    const taskTitle = input.value;
-    if (taskTitle === "") return;
-    const task = document.createElement('li');
-    task.className = 'task';
-    task.innerHTML = `${taskTitle} <button>X</button>`;
-    toDoList.push(task);
-    renderList();
-
-
-    input.value = "";
-    taskNumber.textContent = toDoList.length;
-    task.querySelector('button').addEventListener('click', removeTask);
-
-}
+  input.value = "";
+  taskNumber.textContent = toDoList.length;
+  task.querySelector("button").addEventListener("click", removeTask);
+};
 
 const renderList = () => {
-    ul.textContent = '';
-    toDoList.forEach((toDoElement, key) => {
-        toDoElement.dataset.key = key;
-        ul.appendChild(toDoElement);
-    })
-}
+  ul.textContent = "";
+  toDoList.forEach((toDoElement, key) => {
+    toDoElement.dataset.key = key;
+    ul.appendChild(toDoElement);
+  });
+};
 
-
-form.addEventListener('submit', addTask);
-btnDelete.addEventListener('click', deleteAll);
-inputSearch.addEventListener('input', searchTask);
+form.addEventListener("submit", addTask);
+btnDelete.addEventListener("click", deleteAll);
+inputSearch.addEventListener("input", searchTask);
